7bf5e08e97c257a50747ac66da752e7d36095881
King <kingsp@iu.edu>
2019-08-26 18:28:21 -0400

    Updates the license, README.md, and completes the assignment

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, I am Spencer King.')
+print('The Fallout series is my favorite game.')
+print('I am not concerned for anything in this class.')
+print('I am excited about literally anything involving Virtual Reality.')
+print('My stackoverflow user number is user:11980920.')
+print('My GitHub profile URL is https://github.com/ImKingSP')
